05a8692ef2fbc59fe26bfd4f92c2c563edd03a53
Camca14 <fenixccartosio2@gmail.com>
2024-08-15 08:15:36 -0300

    Update 15/08

---
+++ CarritowebG2/PresentacionAdmin/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClaseEntidades;
+using ClaseBDNegocio;
+
+
+namespace PresentacionAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarUsuario()
+        {
+            List<Usuario> objLista = new List<Usuario>();
+            objLista = new CN_Usuarios().Listar();
+
+            return Json(new {data = objLista }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
