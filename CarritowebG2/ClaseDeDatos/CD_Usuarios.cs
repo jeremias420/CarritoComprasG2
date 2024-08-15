e405bd95e3abb6e490ccaec82c0a2581f2088528
Josse089 <josecamera089@gmail.com>
2024-08-15 10:20:38 -0300

    Update 15/08
    Falta terminar registrar de usuario.cs

--- CarritowebG2/ClaseDeDatos/CD_Usuarios.cs
+++ CarritowebG2/ClaseDeDatos/CD_Usuarios.cs
@@ -53,5 +53,36 @@ namespace ClaseDeDatos
             return lista;
         }
 
+        public int Registrar(Usuario obj, out string Mensaje)
+        {
+            int idautogenerado = 0;
+
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.CN))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarUsuario", oconexion);
+                    cmd.Parameters.AddWithValue("Nombres", obj.usua_Nombre);
+                    cmd.Parameters.AddWithValue("Apellidos", obj.usua_Apellido);
+                    cmd.Parameters.AddWithValue("Correo", obj.usua_Correo);
+                    cmd.Parameters.AddWithValue("Clave", obj.usua_Clave);
+                    cmd.Parameters.AddWithValue("Activo", obj.usua_Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    idautogenerado=Convert.ToInt32(cmd.pa)
+                }
+            }
+
+
+
+        }
+
     }
 }
